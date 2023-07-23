258b3fd6f12db039fc52b922302a07c8c051018c
yaranasserr <yarrymnasser@gmail.com>
2023-07-23 15:13:40 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
-Rails.application.routes.draw do
-  get 'users/new'
-  # get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  root 'static_pages#home'
-  get 'static_pages/contact'
+# Rails.application.routes.draw do
+#   get 'users/new'
+#   # get 'static_pages/home'
+#   get 'static_pages/help'
+#   get 'static_pages/about'
+#   root 'static_pages#home'
+#   get 'static_pages/contact'
+#   get 'users/signup'
 
+#   end
+  Rails.application.routes.draw do
+  get 'users/new'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
   end
-  # Rails.application.routes.draw do
-  #get 'users/new'
-  #   root 'static_pages#home'
-  #   get '/help', to: 'static_pages#help'
-  #   get '/about', to: 'static_pages#about'
-  #   get '/contact', to: 'static_pages#contact'
-  #   end
