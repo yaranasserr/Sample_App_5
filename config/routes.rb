da97a899790351e50126e7f7856eea7dc7ebc8ca
yaranasserr <yarrymnasser@gmail.com>
2023-07-24 12:31:46 +0300

    signup7.1

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,20 @@
 #    get '/signup', to: 'users#new', as: 'signup'
 
 #   end
-  Rails.application.routes.draw do
-  get 'users/new'
-  root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new', as: 'signup'
-  resources :users
-     end
+  # Rails.application.routes.draw do
+  # get 'users/new'
+  # root 'static_pages#home'
+  # get '/help', to: 'static_pages#help'
+  # get '/about', to: 'static_pages#about'
+  # get '/contact', to: 'static_pages#contact'
+  # get '/signup', to: 'users#new', as: 'signup'
+  # resources :users
+  #    end
+     Rails.application.routes.draw do
+      root 'static_pages#home'
+      get '/help', to: 'static_pages#help'
+      get '/about', to: 'static_pages#about'
+      get '/contact', to: 'static_pages#contact'
+      get '/signup', to: 'users#new'
+      resources :users
+      end
