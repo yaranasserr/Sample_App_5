3fa2f88d625c8a8313ddc602373c2c789f34cde2
yaranasserr <yarrymnasser@gmail.com>
2023-07-24 18:16:37 +0300

    loginn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  # def logged_in?
+  #   # Add the implementation for checking if the user is logged in
+  #   # For example, you might have something like:
+  #   # current_user.present?
+  # end
   end
