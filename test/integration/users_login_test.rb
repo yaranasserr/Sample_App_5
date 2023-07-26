e301945f11166c1974a6fd98e07902ac87527f3f
yaranasserr <yarrymnasser@gmail.com>
2023-07-26 15:35:37 +0300

    10.2

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -24,19 +24,6 @@ class InvalidPasswordTest < UsersLogin
     get root_path
     assert flash.empty?
   end
-
-  test "login with remembering" do
-  log_in_as(@user, remember_me: '1')
-  assert_equal FILL_IN, assigns(:user).FILL_IN
-  end
-
-test "login without remembering" do
-# Log in to set the cookie.
-  log_in_as(@user, remember_me: '1')
-  # Log in again and verify that the cookie is deleted.
-  log_in_as(@user, remember_me: '0')
-  assert_empty cookies[:remember_token]
-  end
 end
 
 class ValidLogin < UsersLogin
@@ -87,24 +74,24 @@ class LogoutTest < Logout
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
-#   test "should still work after logout in second window" do
-#     delete logout_path
-#     assert_redirected_to root_url
-#   end
-# end
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
 
-# class RememberingTest < UsersLogin
+class RememberingTest < UsersLogin
 
-#   test "login with remembering" do
-#     log_in_as(@user, remember_me: '1')
-#     assert_not cookies[:remember_token].blank?
-#   end
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
 
-#   test "login without remembering" do
-#     # Log in to set the cookie.
-#     log_in_as(@user, remember_me: '1')
-#     # Log in again and verify that the cookie is deleted.
-#     log_in_as(@user, remember_me: '0')
-#     assert cookies[:remember_token].blank?
-#   end
-# end
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
