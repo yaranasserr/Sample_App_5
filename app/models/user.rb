a8240d1c5aa7f88947c3589cf32de5ee46631fc7
yaranasserr <yarrymnasser@gmail.com>
2023-07-30 13:59:43 +0300

    password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
@@ -33,23 +33,43 @@ class User < ApplicationRecord
   end
 
     # Returns true  the given token matches the digest.
-    def authenticated?(attribute, token)
+  def authenticated?(attribute, token)
       digest = send("#{attribute}_digest")
       return false if digest.nil?
       BCrypt::Password.new(digest).is_password?(token)
-      end
+  end
 
-    def forget
+  def forget
       return false if remember_digest.nil?
       update_attribute(:remember_digest, nil)
-    end
+  end
+
     # Activates an account.
-    def activate
-      update_columns(activated: FILL_IN, activated_at: FILL_IN)
-      end
+  def activate
+      # update_attribute(:activated, true)
+      # update_attribute(:activated_at, Time.zone.now)
+      update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
-    end
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    # update_attribute(:reset_digest, User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: FILL_IN, reset_sent_at: FILL_IN)
+  end
+
+    # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
 # Converts email to all lower-case.
 private
@@ -57,9 +77,9 @@ private
     self.email = email.downcase
     end
 
-    def create_activation_digest
+  def create_activation_digest
       self.activation_token = User.new_token
       self.activation_digest = User.digest(activation_token)
-    end
+  end
 
 end
